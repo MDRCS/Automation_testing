7bac2a54a6ba1768430fc522b09431df46360e28
MDRCS <elrahali.md@gmail.com>
2020-06-18 22:10:35 +0100

    system testing

--- TDD_Coding/tests/system/app_test.py
+++ TDD_Coding/tests/system/app_test.py
@@ -6,6 +6,18 @@ from TDD_Coding.blog import Blog
 
 class AppTest(TestCase):
 
+    def test_menu_input_selection(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    def test_menu_call_print_blogs(self):
+        with patch('TDD_Coding.app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):
+                app.menu()
+                mocked_print_blogs.assert_called_with()
+
+
     def test_print_blogs(self):
         b = Blog('Test', 'mdrahali')
         app.blogs = {'Test': b}
@@ -14,3 +26,12 @@ class AppTest(TestCase):
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- blog Test by mdrahali')
+
+
+    def test_create_blog_input(self):
+        with patch('builtins.input') as mocked_input:
+            # side_effect func help us insert those values in the
+            # two input that we have
+            mocked_input.side_effect = ('Test', 'Test Author')
+            app.ask_create_blog()
+            self.assertIsNotNone(app.blogs.get('Test'))
