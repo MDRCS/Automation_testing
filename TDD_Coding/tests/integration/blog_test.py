72177ebefb9573f24cff6b75a1dca466c27406e1
MDRCS <elrahali.md@gmail.com>
2020-06-18 19:29:20 +0100

    Blog Integration tests

--- TDD_Coding/tests/integration/blog_test.py
+++ TDD_Coding/tests/integration/blog_test.py
@@ -13,3 +13,18 @@ class BlogTest(TestCase):
 
         self.assertEqual(p.title, b.posts[0].title)
 
+    def test_json(self):
+        b = Blog('Coding', 'mdrahali')
+        b.create_post('how works kub8', 'kub8 orchestration tool')
+        expected = {
+                    'title': b.title,
+                    'author': b.author,
+                    'posts':
+                        [
+                            {
+                                'title': b.posts[0].title,
+                                'content': b.posts[0].content
+                            }
+                        ]
+                    }
+        self.assertDictEqual(b.json(), expected)
