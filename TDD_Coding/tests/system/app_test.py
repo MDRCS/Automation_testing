73b4f626b3c3581f54d54cdf3fde5edc3f3ee873
MDRCS <elrahali.md@gmail.com>
2020-06-18 23:42:07 +0100

    syetem tests - adding setUp

--- TDD_Coding/tests/system/app_test.py
+++ TDD_Coding/tests/system/app_test.py
@@ -7,8 +7,12 @@ from TDD_Coding.post import Post
 
 class AppTest(TestCase):
 
+    def setUp(self) -> None:
+        blog = Blog('Test', 'mdrahali')
+        app.blogs = {'Test': blog}
+
     def test_menu_input_selection(self):
-        with patch('builtins.input') as mocked_input:
+        with patch('builtins.input', return_value='q') as mocked_input:
             app.menu()
             mocked_input.assert_called_with(app.MENU_PROMPT)
 
@@ -19,9 +23,6 @@ class AppTest(TestCase):
                 mocked_print_blogs.assert_called_with()
 
     def test_print_blogs(self):
-        b = Blog('Test', 'mdrahali')
-        app.blogs = {'Test': b}
-        app.print_blogs()
         # patch function allows us to copy a function is behaviour and test the result if it's the same or not
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
@@ -36,37 +37,27 @@ class AppTest(TestCase):
             self.assertIsNotNone(app.blogs.get('Test'))
 
     def test_read_bog(self):
-        blog = Blog('Test', 'mdrahali')
-        blog.create_post('test', 'test content')
-        app.blogs = {'Test': blog}
-
+        app.blogs['Test'].create_post('test', 'test content')
         with patch('builtins.input', return_value='Test'):
             with patch('TDD_Coding.app.print_posts') as mocked_print:
                 app.ask_read_blog()
 
-                mocked_print.assert_called_with(blog)
+                mocked_print.assert_called_with(app.blogs['Test'])
 
     def test_print_posts(self):
-        blog = Blog('Test', 'mdrahali')
-        blog.create_post('test', 'test content')
-        app.blogs = {'Test': blog}
-
+        app.blogs['Test'].create_post('test', 'test content')
         with patch('TDD_Coding.app.print_post') as mocked_print_post:
-            app.print_posts(blog)
-            mocked_print_post.assert_called_with(blog.posts[0])
+            app.print_posts(app.blogs['Test'])
+            mocked_print_post.assert_called_with(app.blogs['Test'].posts[0])
 
     def test_print_post(self):
         post = Post('test', 'test content')
 
         with patch('builtins.print') as mocked_print:
             app.print_post(post)
-
             mocked_print.assert_called_with(app.POST_TEMPLATE.format(post.title, post.content))
 
     def test_create_post(self):
-        blog = Blog('Test', 'mdrahali')
-        app.blogs = {'Test': blog}
-
         with patch('builtins.input') as mocked_input:
             mocked_input.side_effect = ('Test', 'Test', 'Test Content')
             app.ask_create_post()
